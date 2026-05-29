92616d279329b30c7197a36ccb77d85f357bd72a
Stefano Moia <s.moia.research@gmail.com>
2026-05-29 17:55:51 +0100

    Add two break tests

--- phys2bids/tests/test_slice4phys.py
+++ phys2bids/tests/test_slice4phys.py
@@ -19,3 +19,13 @@ def test_estimate_ntp_and_tr(multi_run_file):
 
     assert est_ntp == [534, 513]
     assert est_tr == [1.2, 1.2]
+
+    # BREAK
+    with raises(ValueError) as errorinfo:
+        est_ntp, est_tr = slice4phys.estimate_ntp_and_tr(phys_obj, ci=1.20)
+    assert "percentages above 1" in str(errorinfo.value)
+    with raises(ValueError) as errorinfo:
+        est_ntp, est_tr = slice4phys.estimate_ntp_and_tr(phys_obj, ci="left")
+    assert "integers or floats" in str(errorinfo.value)
+
+    # Add test on single take file → maybe as integration test.
